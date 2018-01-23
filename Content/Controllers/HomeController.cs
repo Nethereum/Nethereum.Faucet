862e4c647e006d9fd041d428cc267bae7c29b57d
Juan Blanco <juanfranblanco@hotmail.com>
2018-01-23 13:51:38 -0800

    Upgrade

--- Content/Controllers/HomeController.cs
+++ Content/Controllers/HomeController.cs
@@ -35,13 +35,13 @@ namespace NethereumFaucet.Controllers
                 var web3 = new Web3(account, settings.EthereumAddress);
 
                 var balance = await web3.Eth.GetBalance.SendRequestAsync(faucetViewModel.Address);
-                if (web3.Convert.FromWei(balance.Value) > settings.MaxAmountToFund)
+                if (Web3.Convert.FromWei(balance.Value) > settings.MaxAmountToFund)
                 {
                     ModelState.AddModelError("address", "Account cannot be funded, already has more than " + settings.MaxAmountToFund + " ether");
                     return View(faucetViewModel);
                 }
 
-                var txnHash = await web3.Eth.TransactionManager.SendTransactionAsync(account.Address, faucetViewModel.Address, new HexBigInteger(web3.Convert.ToWei(settings.AmountToFund)));
+                var txnHash = await web3.Eth.TransactionManager.SendTransactionAsync(account.Address, faucetViewModel.Address, new HexBigInteger(Web3.Convert.ToWei(settings.AmountToFund)));
                 faucetViewModel.TransactionHash = txnHash;
                 faucetViewModel.Amount = settings.AmountToFund;
                 
